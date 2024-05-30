6bfc9c7fc131b92531c045e519fdf5afe4656f8a
tiancong li <litiancong@hotmail.com>
2024-05-30 17:55:48 +1000

    create main class and list methods in tap processor

--- src/main/java/com/littlepay/Main.java
+++ src/main/java/com/littlepay/Main.java
@@ -1,8 +1,16 @@
 package main.java.com.littlepay;
 
+import java.util.List;
+
 /**
  * @author ltiancong@gmail.com
  * @date 2024/5/30 13:05
  */
 public class Main {
+    public static void main(String[] args) {
+        List<Tap> taps = TapProcessor.importTapsFrom("src/main/resources" +
+                "/taps.csv");
+        List<Trip> trips = TapProcessor.generateTripsFrom(taps);
+        TapProcessor.exportTripsTo("src/main/resources/trips.csv", trips);
+    }
 }
--- src/main/java/com/littlepay/TapProcessor.java
+++ src/main/java/com/littlepay/TapProcessor.java
@@ -1,8 +1,21 @@
 package main.java.com.littlepay;
 
+import java.util.List;
+
 /**
  * @author ltiancong@gmail.com
  * @date 2024/5/30 13:05
  */
 public class TapProcessor {
+    public static List<Tap> importTapsFrom(String s) {
+        return null;
+    }
+
+
+    public static List<Trip> generateTripsFrom(List<Tap> taps) {
+        return null;
+    }
+
+    public static void exportTripsTo(String s, List<Trip> trips) {
+    }
 }
